afa7419d95195cc1b41850bb986ddc0014ccb9a8
VYWL <dksu40@gmail.com>
2023-10-03 16:00:35 +0900

    [fix] Customizing 진행중

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    name: "GDSC Hanyang",
+    image: "/gdsc_hyu_logo.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "GDSC 한양대 팀 블로그",
+    bio: "한양대 팀 블로그 입니다!",
+    email: "hanyanggdsc@gmail.com",
+    linkedin: "",
+    github: "GDSC-Hanyang",
+    instagram: "@gdsc_hanyang",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `GDSC Hanyang landing page`,
+      href: "https://gdsc-hanyang.oopy.io/",
     },
   ],
   // blog setting (required)
   blog: {
     title: "morethan-log",
-    description: "welcome to morethan-log!",
+    description: "welcome to morethan-log! ss",
   },
 
   // CONFIG configration (required)
